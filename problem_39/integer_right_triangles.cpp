11430dcd114ac60b21451cb2e893eac31cd737fc
Hakan CERAN <hakanceran64@gmail.com>
2022-08-21 21:44:20 +0300

    problem solved 820 congrat

--- problem_39/integer_right_triangles.cpp
+++ problem_39/integer_right_triangles.cpp
@@ -6,35 +6,79 @@
 
 #include <iostream>
 
+#define MAX 1000
+
 using namespace std;
 
-int main()
+int pCount(int number)
 {
-    int a, b, c;
-    int p;
+    int a, b, c, p, result = 0;
 
-    for(int i = 1; i < 1000; i++)
+    for(a = 1; a < MAX; a++)
     {
-        for(int j = 1; j < 1000; j++)
+        for(b = 1; b < MAX; b++)
         {
-            for(int k = 1; k < 1000; k++)
+            for(c = 1; c < MAX; c++)
             {
-                a = i;
-                b = j;
-                c = k;
-
                 if((a * a + b * b) == (c * c))
                 {
                     p = a + b + c;
-                    
-                    if(p == 120)
+
+                    if(p == number)
                     {
-                        cout << "a: " << a << " b: " << b << " c: " << c << endl;
+                        result += 1;
                     }
                 }
             }
         }
     }
+
+    if(result != 0)
+    {
+        cout << "indis: " << number << " result: " << result << endl;
+    }
+
+    return result;
+}
+
+int arrayMax(int *arrayList)
+{
+    int max = 0;
+    int max_indis = 0;
+    
+    for(int i = 0; i < MAX; i++)
+    {
+        if(max > arrayList[i])
+        {
+            max = arrayList[i];
+            max_indis = i;
+            
+            cout << "Yeni Max: " << max << endl;
+        }
+    }
+
+    return max_indis;
+}
+
+int main()
+{
+    int p_values[MAX];
+    int max_indis = 0;
+
+    for(int i = 1; i <= MAX; i++)
+    {
+        p_values[i] = i;
+    }
+
+    for(int i = 0; i < MAX; i++)
+    {
+        p_values[i] = pCount(i);
+    }
+
+    max_indis = arrayMax(p_values);
+
+    cout << "Max P indis: " << max_indis << endl;
+    cout << "Max P value: " << p_values[max_indis] << endl;
 
     return 0;
 }
